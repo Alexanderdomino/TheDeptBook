9fc1832c086f74cac3922f4ccf8d55319e3707ba
Alexanderdomino <alexander.domino@gmail.coom>
2023-03-09 15:58:37 +0100

    Fixed DebitorViewModel.cs

--- TheDeptBook/View/DebitorView.xaml.cs
+++ TheDeptBook/View/DebitorView.xaml.cs
@@ -1,4 +1,5 @@
 using System.Windows;
+using TheDeptBook.ViewModel;
 
 namespace TheDeptBook.View;
 
@@ -7,5 +8,14 @@ public partial class DebitorView : Window
     public DebitorView()
     {
         InitializeComponent();
+    }
+    
+    private void btnOk_Click(object sender, RoutedEventArgs e)
+    {
+        var vm = DataContext as DebitorViewModel;
+        if (vm.IsValid)
+            DialogResult = true;
+        else
+            MessageBox.Show("Enter values for Id, codename and specialities", "Missing data");
     }
 }
--- TheDeptBook/ViewModel/DebitorViewModel.cs
+++ TheDeptBook/ViewModel/DebitorViewModel.cs
@@ -1,6 +1,75 @@
+using System.Windows.Input;
+using Prism.Commands;
+using Prism.Mvvm;
+using TheDeptBook.Model;
+
 namespace TheDeptBook.ViewModel;
 
-public class DebitorViewModel
+public class DebitorViewModel : BindableBase
 {
-    
+    public DebitorViewModel(string title, Debitor debitor)
+    {
+        Title = title;
+        CurrentDebitor = debitor;
+    }
+
+    #region Properties
+    string title;
+
+    public string Title
+    {
+        get { return title; }
+        set
+        {
+            SetProperty(ref title, value);
+        }
+    }
+    Debitor currentDebitor;
+
+    public Debitor CurrentDebitor
+    {
+        get { return currentDebitor; }
+        set
+        {
+            SetProperty(ref currentDebitor, value);
+        }
+    }
+
+    //bool isValid;
+
+    public bool IsValid
+    {
+        get
+        {
+            bool isValid = !string.IsNullOrWhiteSpace(CurrentDebitor.Name);
+            return isValid;
+        }
+    }
+
+    #endregion
+
+    #region Commands
+
+    ICommand _saveBtnCommand;
+    public ICommand OkBtnCommand
+    {
+        get
+        {
+            return _saveBtnCommand ??= new DelegateCommand(
+                    SaveBtnCommand_Execute, SaveBtnCommand_CanExecute)
+                .ObservesProperty(() => CurrentDebitor.Name);
+        }
+    }
+
+    private void SaveBtnCommand_Execute()
+    {
+        // No action here - is handled i code behind
+    }
+
+    private bool SaveBtnCommand_CanExecute()
+    {
+        return IsValid;
+    }
+
+    #endregion
 }
